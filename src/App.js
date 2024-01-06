efd6be1c760c32c385bf8b458911759803944e91
Biswajit30 <biswajitjena0611@gmail.com>
2024-01-06 09:04:05 +0530

    Update Credit Card using react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreditCardForm from './components/creditcards/CreditCardForm';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container mt-5">
+      <h2 className="mb-4">React Animated Credit Card UI Example</h2>
+      <CreditCardForm />
     </div>
   );
 }
